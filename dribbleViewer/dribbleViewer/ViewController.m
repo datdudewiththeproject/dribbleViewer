7ea26d458164b546f0f74fc34e53c782f7199e50
Andrew Sevrikov <devandsev@gmail.com>
2016-02-16 20:06:44 +0300

    added project

---
+++ dribbleViewer/dribbleViewer/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  dribbleViewer
+//
+//  Created by admin on 16/02/16.
+//  Copyright © 2016 sevrikov. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ dribbleViewer/dribbleViewer/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  dribbleViewer
+//
+//  Created by admin on 16/02/16.
+//  Copyright © 2016 sevrikov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
