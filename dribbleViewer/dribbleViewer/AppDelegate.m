48d93832a27f2ab5aceae041f6beeae2544166ba
Andrew Sevrikov <devandsev@gmail.com>
2016-02-16 22:07:12 +0300

    Core Data stack init and RestKit base URL

--- dribbleViewer/dribbleViewer/AppDelegate.m
+++ dribbleViewer/dribbleViewer/AppDelegate.m
@@ -8,6 +8,9 @@
 
 #import "AppDelegate.h"
 
+#import <RestKit/CoreData.h>
+#import <RestKit/RestKit.h>
+
 @interface AppDelegate ()
 
 @end
@@ -16,6 +19,24 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    NSURL *baseURL = [NSURL URLWithString:@"https://api.dribbble.com/v1"];
+    RKObjectManager *objectManager = [RKObjectManager managerWithBaseURL:baseURL];
+    
+    // Core Data stack initialization
+    NSManagedObjectModel *managedObjectModel = [NSManagedObjectModel mergedModelFromBundles:nil];
+    RKManagedObjectStore *managedObjectStore = [[RKManagedObjectStore alloc] initWithManagedObjectModel:managedObjectModel];
+    objectManager.managedObjectStore = managedObjectStore;
+    
+    [managedObjectStore createPersistentStoreCoordinator];
+    NSString *storePath = [RKApplicationDataDirectory() stringByAppendingPathComponent:@"dribbleDB.sqlite"];
+    NSString *seedPath = [[NSBundle mainBundle] pathForResource:@"RKSeedDatabase" ofType:@"sqlite"];
+    NSError *error;
+    NSPersistentStore *persistentStore = [managedObjectStore addSQLitePersistentStoreAtPath:storePath fromSeedDatabaseAtPath:seedPath withConfiguration:nil options:nil error:&error];
+    NSAssert(persistentStore, @"Failed to add persistent store with error: %@", error);
+    
+    [managedObjectStore createManagedObjectContexts];
+    
+    managedObjectStore.managedObjectCache = [[RKInMemoryManagedObjectCache alloc] initWithManagedObjectContext:managedObjectStore.persistentStoreManagedObjectContext];
     
     return YES;
 }
