6daab18eff81e87f8867371d3bfed8e7efac30f3
Andrew Sevrikov <devandsev@gmail.com>
2016-02-16 21:32:12 +0300

    changed project structure

--- dribbleViewer/dribbleViewer/Controllers/ShotsViewController.h
+++ dribbleViewer/dribbleViewer/Controllers/ShotsViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  ShotsViewController.h
 //  dribbleViewer
 //
 //  Created by admin on 16/02/16.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface ShotsViewController : UICollectionViewController
 
 @end
-
---
+++ dribbleViewer/dribbleViewer/Controllers/ShotsViewController.m
@@ -0,0 +1,13 @@
+//
+//  ShotsViewController.m
+//  dribbleViewer
+//
+//  Created by admin on 16/02/16.
+//  Copyright © 2016 sevrikov. All rights reserved.
+//
+
+#import "ShotsViewController.h"
+
+@implementation ShotsViewController
+
+@end
